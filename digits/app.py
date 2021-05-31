2cea667a240b83fb5eba40e7d69bc45710a111e4
wizardcalidad <yusufqoyum01@gmail.com>
2021-06-01 00:58:47 +0100

    new commit

--- digits/app.py
+++ digits/app.py
@@ -31,14 +31,60 @@ async def digit_image(file: UploadFile = File(...)):
 @app.get("/")
 async def index():
     content = """
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
+</head>
 <body>
-<navbar colour="blue">A digit recognizer model</navbar>
-<form action="/predict_upload_file/" enctype="multipart/form-data" method="post">
+
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#about">About</a>
+</div>
+
+<div style="padding-left:16px">
+  <h2>Digit Recognition app</h2>
+  <p>Insert your image here</p>
+  <form action="/predict_upload_file/" enctype="multipart/form-data" method="post">
 <input name="file" type="file" multiple>
+</br></br>
 <input type="submit">
 </form>
-<footer>by @wizardcalidad</footer>
+</div>
+
 </body>
+</html>
+
     """
 
     return HTMLResponse(content=content)
